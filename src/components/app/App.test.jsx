fcb464ceeb99321abfee35eab261315d954b6e7b
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-16 14:30:11 -0800

    adds passing undo test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -20,4 +20,26 @@ describe('App component', () => {
       backgroundColor: '#FF0000'
     });
   });
+
+  it('returns to previous color via the undo', () => {
+    render(<App />);
+
+    const undoClick = screen.getByText('undo');
+    const colorInput = screen.getByLabelText('Color');
+    const displayColor = screen.getByTestId('display');
+
+    fireEvent.change(colorInput, {
+      target: {
+        values: '#00FF00'
+      }
+    });
+
+    fireEvent.click(undoClick);
+
+    expect(displayColor).toHaveStyle({
+      backgroundColor: '#FF0000'
+    });
+  });
+
+  
 });
