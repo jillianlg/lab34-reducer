2c99332b240116cad3ca8781e3c45e50d71f306a
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-16 14:30:41 -0800

    add passing redo test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -41,5 +41,25 @@ describe('App component', () => {
     });
   });
 
-  
+  it('returns to previous color via the redo', () => {
+    render(<App />);
+
+    const redoClick = screen.getByText('redo');
+    const undoClick = screen.getByText('undo');
+    const colorInput = screen.getByLabelText('Color');
+    const displayColor = screen.getByTestId('display');
+
+    fireEvent.change(colorInput, {
+      target: {
+        values: '#00FF00'
+      }
+    });
+
+    fireEvent.click(undoClick);
+    fireEvent.click(redoClick);
+
+    expect(displayColor).toHaveStyle({
+      backgroundColor: '#FF0000'
+    });
+  });
 });
